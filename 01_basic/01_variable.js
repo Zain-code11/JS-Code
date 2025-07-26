8ee8674c50a6188a3c6086160b40614bbd66ee63
Zain Hassan <147242840+Zain-code11@users.noreply.github.com>
2025-07-26 04:16:36 +0000

    config change

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const userId = 123456;
+let userGmail = 'Zain@gmail.com';
+var password = 'Zain123';
+
+console.log(userId);
+console.log(userGmail);
+console.log(password);
+
+userGmail = '1111';
+console.log(userGmail)
+
+var password = '1111122222223333333'
+console.log(password)
